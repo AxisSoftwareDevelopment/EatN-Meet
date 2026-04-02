3da7e5de7fc5a0a4ef3e2e973f43f4029d118226
GODston <80297897+GODston@users.noreply.github.com>
2026-04-02 02:12:47 -0600

    Fixing aspect ratio of spot picture and like button on profile praises listview

--- Spots/Views/Users/CP_UserProfile.xaml.cs
+++ Spots/Views/Users/CP_UserProfile.xaml.cs
@@ -119,23 +119,26 @@ public partial class CP_UserProfile : ContentPage
 
     private async void LikeButtonClicked(object sender, EventArgs e)
     {
+        ((ImageButton)sender).IsEnabled = false;
         if (SessionManager.CurrentSession?.Client != null)
         {
-            bool? likedState = await ((SpotPraise)((Button)sender).BindingContext).LikeSwitch(SessionManager.CurrentSession.Client.UserID);
+            bool? likedState = await ((SpotPraise)((ImageButton)sender).BindingContext).LikeSwitch(SessionManager.CurrentSession.Client.UserID);
 
-            if (likedState != null )
+            if (likedState != null)
             {
-                if((bool)likedState)
+                if ((bool)likedState)
                 {
-                    ((SpotPraise)((Button)sender).BindingContext).Likes.Add(SessionManager.CurrentSession.Client.UserID);
-                    ((SpotPraise)((Button)sender).BindingContext).LikesCount++;
+                    ((SpotPraise)((ImageButton)sender).BindingContext).Likes.Add(SessionManager.CurrentSession.Client.UserID);
+                    ((SpotPraise)((ImageButton)sender).BindingContext).LikesCount++;
+
                 }
                 else
                 {
-                    ((SpotPraise)((Button)sender).BindingContext).Likes.Remove(SessionManager.CurrentSession.Client.UserID);
-                    ((SpotPraise)((Button)sender).BindingContext).LikesCount--;
+                    ((SpotPraise)((ImageButton)sender).BindingContext).Likes.Remove(SessionManager.CurrentSession.Client.UserID);
+                    ((SpotPraise)((ImageButton)sender).BindingContext).LikesCount--;
                 }
             }
         }
+        ((ImageButton)sender).IsEnabled = true;
     }
 }
